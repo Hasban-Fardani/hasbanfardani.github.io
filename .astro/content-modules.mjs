ea2bcb22793f69e5270f6b1499917c4dee09d837
Hasban-Fardani <hasbanfardani77@gmail.com>
2025-07-03 16:19:37 +0700

    fix: github workflows

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -7,6 +7,6 @@ export default new Map([
 ["src/content/projects/5.competitive-programmings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F5.competitive-programmings.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/6.csr-goverment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F6.csr-goverment.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/7.HF-Store.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F7.HF-Store.mdx&astroContentModuleFlag=true")],
-["src/content/projects/8.pelita-taras.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F8.pelita-taras.mdx&astroContentModuleFlag=true")],
-["src/content/projects/9.simple-finance-apps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F9.simple-finance-apps.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/9.simple-finance-apps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F9.simple-finance-apps.mdx&astroContentModuleFlag=true")],
+["src/content/projects/8.pelita-taras.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F8.pelita-taras.mdx&astroContentModuleFlag=true")]]);
 		
